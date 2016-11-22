ab3875e8b9b3a34452734a7ab3bf7854223554a8
Felipe Siqueira <fel.siqueirapinheiro@gmail.com>
2016-11-22 17:53:23 -0200

    Nothing again

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(window).resize("resizeBackground");
 function resizeBackground() {
   teste.height($(window).height());
 }
-
+resizeBackground();
 $(document).ready(function() {
   $(".button-collapse").sideNav({
     draggable: true
