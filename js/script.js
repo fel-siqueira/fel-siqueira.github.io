5ece52c4f99f9557b3923f7d7736789f73cbb52d
Felipe Siqueira <fel.siqueirapinheiro@gmail.com>
2016-11-22 21:55:59 -0200

    nada

--- js/script.js
+++ js/script.js
@@ -6,7 +6,9 @@ $(document).ready(function() {
   var initialHeight = $(window).height();
 
   $(window).scroll(function(initialHeight) {
-    $(this).height(initialHeight);
+    var diff = Math.abs(initialHeight - $(this).height());
+
+    $(this).height(initialHeight+diff);
 
     
   });
