455bfa761cd01feb65a6b69e0f3cd826bb140e8f
Felipe Siqueira <fel.siqueirapinheiro@gmail.com>
2016-11-22 16:38:09 -0200

    nothing

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-var hd = $("header");
-$(window).resize("resizeBackground");
-
-function resizeBackground() {
-  hd.height($(window).height() + 60);  
-}
-
-resizeBackground();
-
 $(document).ready(function() {
   $(".button-collapse").sideNav({
     draggable: true
