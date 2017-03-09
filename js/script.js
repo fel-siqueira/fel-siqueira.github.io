dfed60a72e955ad1676e4c56396ea4e6b8e6d470
Felipe Siqueira Pinheiro <fel.siqueirapinheiro@gmail.com>
2017-03-08 21:12:59 -0300

    Rebooted

--- js/script.js
+++ js/script.js
@@ -2,11 +2,6 @@ $(document).ready(function() {
   $(".button-collapse").sideNav({
     draggable: true
   });
-
-  var inicial = $(window).height();
-  var queroqueseja = inicial + 54;
-  $("header").height(queroqueseja);
-
 });
 
 
