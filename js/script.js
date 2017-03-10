6afb8396fba2581d4b0fb796f6bdf3be23aff169
Felipe Siqueira Pinheiro <fel.siqueirapinheiro@gmail.com>
2017-03-10 01:27:25 -0300

    Added an AJAX request using JQuery

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
+
+  // MaterializeCSS
   $(".button-collapse").sideNav({
     draggable: true
   });
-});
 
+  // JQuery
+  $.ajax({
+    url: "https://api.github.com/users/fel-siqueira", 
+    success: function(result){ 
+      $("#picture").attr("src", result.avatar_url);
+    }
+  });
+});
 
