64c186b175095eba8116ae6d3d638c5d34a9189c
Felipe Siqueira <fel.siqueirapinheiro@gmail.com>
2017-03-12 22:57:03 -0300

    Almost there

--- js/script.js
+++ js/script.js
@@ -9,25 +9,28 @@ $(document).ready(function() {
     draggable: true
   });
 
-  // JQuery
+  $('.dropdown-button + .dropdown-content').on('click touchstart', function(event) {
+    event.stopPropagation();
+  });
 
+  // JQuery
   $.ajax({
     url: 'https://api.github.com/users/fel-siqueira/repos',
     success: function(result) {
       for(var i = 0; i < result.length; i++) {
         var repos = ['learnGo', 'toneAnalyzerExample', 'felguigofel', 'fel-siqueira.github.io', 'curso-js-fatec', 'ZnoteAAC'];
-        var card = '<div class="col s12 m4">\
-                <div class="card small grey lighten-5 z-depth-3">\
-                  <div class="card-content blue-grey-text text-darken-4">\
-                    <span class="card-title">'+ result[i].name + ' </span>\
-                    <p>'+ result[i].description +'</p>\
-                  </div>\
-                  <div class="card-action">\
-                    <a href="'+ result[i].html_url+'" target="_blank">REPO</a>\
-                    <span class="right blue-grey-text">'+ result[i].language +'</span>\
-                  </div>\
-                </div>\
-              </div>'
+        var card = '<div class="col s12 m4">' +
+                   '  <div class="card small grey lighten-5 z-depth-3">' +
+                   '    <div class="card-content blue-grey-text text-darken-4">' +
+                   '      <span class="card-title">'+ result[i].name + ' </span>' +
+                   '      <p>'+ result[i].description +'</p>' +
+                   '    </div>' +
+                   '    <div class="card-action">' +
+                   '      <a href="'+ result[i].html_url+'" target="_blank">REPO</a>' +
+                   '      <span class="right blue-grey-text">'+ result[i].language +'</>' +
+                   '    </div>' +
+                   '  </div>' +
+                   '</div>'
         if ($.inArray(result[i].name, repos) != -1) {
           $('#repos').append(card);
         }
@@ -37,16 +40,13 @@ $(document).ready(function() {
   $.ajax({
     url: 'https://api.github.com/users/fel-siqueira',
     success: function(result) {
-      var profile = '<img id="picture" class="responsive-img" src="'+ result.avatar_url +'" alt="">\
-                     <div id="name-about">\
-                       <h1 class="flow-text blue-grey-text text-lighten-5">'+ result.name +'</h1>\
-                       <p id="aboutme" class="flow-text blue-grey-text text-lighten-5">'+ result.bio +'</p>\
-                     </div>'
+      var profile = '<img id="picture" class="responsive-img" src="'+ result.avatar_url +'" alt="">' +
+                    '<div id="name-about">' +
+                    '  <h1 class="flow-text blue-grey-text text-lighten-5">'+ result.name +'</h1>' +
+                    '  <p id="aboutme" class="flow-text blue-grey-text text-lighten-5">'+ result.bio +'</p>' +
+                    '</div>'
       $('#myprofile').append(profile);
     },
-  });
-    $('.dropdown-button + .dropdown-content').on('click touchstart', function(event) {
-    event.stopPropagation();
   });
 });
 
